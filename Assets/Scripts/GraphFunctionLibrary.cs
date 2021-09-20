522cf10dff6b7a57380c9745fec2ee6f86eff58f
616games <86506015+616games@users.noreply.github.com>
2021-09-20 15:38:11 -0400

    Update GraphFunctionLibrary.cs
    Removed unused methods HorizontalLine and SlopedLine.

--- Assets/Scripts/GraphFunctionLibrary.cs
+++ Assets/Scripts/GraphFunctionLibrary.cs
@@ -4,21 +4,6 @@ public class GraphFunctionLibrary : MonoBehaviour
 {
     #region --Custom Methods--
     
-    private static Vector3 HorizontalLine(float _xInput, bool _positiveExponent, float _coefficient = 1f, float _yIntercept = 0f)
-    {
-        float _yInput = _coefficient + _yIntercept;
-        return new Vector3(_xInput, _yInput, 0);
-    }
-    
-    /// <summary>
-    /// Simulates the graph of a line.
-    /// </summary>
-    private static Vector3 SlopedLine(float _xInput, bool _positiveExponent, float _coefficient = 1f, float _yIntercept = 0f)
-    {
-        float _yInput = _coefficient * (_positiveExponent ? _xInput : -_xInput) + _yIntercept;
-        return new Vector3(_xInput, _yInput, 0);
-    }
-
     /// <summary>
     /// Helper method for parametric functions.
     /// </summary>
