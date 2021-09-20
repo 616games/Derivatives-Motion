47712ddf73d056be377f90090aeb745c96ec2779
616games <86506015+616games@users.noreply.github.com>
2021-09-20 15:36:28 -0400

    Updated Graph.cs
    Created field to control graph Y position boundary (_yPositionBoundary).
    Updated various comments for better readability.
    Set the _previousCount field to -1 on Init and ResetGraph to ensure its value will always be less than _resetCounter = 0.
    Inverted if statement in CheckDerivativeCount method.
    Renamed temp Vector3 _updatedX to _updatedInput.
    Changed the check against the _derivative.exponent to <= 0 instead of Mathf.Epsilon in the CalculateDerivative method.

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -8,13 +8,16 @@ public class Graph : MonoBehaviour
     [SerializeField, Tooltip("Global counter to monitor when all graphs should be reset.")]
     private Counter _resetCounter;
 
+    [SerializeField, Tooltip("Y position desired to stop the graph from drawing when it passes this value.")]
+    private float _yPositionBoundary;
+
     [SerializeField, Tooltip("The original equation for this graph to which any derivatives will be created from.")]
     private GraphFunctionEquation _originalFunction;
 
-    [SerializeField, Tooltip("The number of derivatives to be taken of the _originalFunction.")]
+    [SerializeField, Tooltip("The number of derivatives to be taken of the _originalFunction (n).")]
     private int _derivativeCount;
     
-    [SerializeField, Tooltip("The derivative of the _originalFunction.")]
+    [SerializeField, Tooltip("The nth derivative of the _originalFunction.")]
     private GraphFunctionEquation _derivativeFunction;
     
     [SerializeField, Tooltip("How fast the graph should be drawn.")]
@@ -25,6 +28,7 @@ public class Graph : MonoBehaviour
 
     /// <summary>
     /// Keeps track of the change in value of the _resetCounter to determine when a reset should occur.
+    /// Reset should occur when the _resetCounter stops incrementing (when _previousCount = _resetCounter).
     /// </summary>
     private int _previousCount;
 
@@ -44,7 +48,7 @@ public class Graph : MonoBehaviour
     private bool _isResetting;
 
     /// <summary>
-    /// Input value for the graph function.
+    /// Input value (x in f(x)) for the graph function.
     /// </summary>
     private float _input;
 
@@ -101,6 +105,7 @@ public class Graph : MonoBehaviour
         _transform = transform;
         _startingPosition = _transform.parent.position;
         _resetCounter = Resources.Load("ScriptableObjects/ResetCounter") as Counter;
+        _previousCount = -1;
         
         ConfigureLineRenderer();
         CheckDerivativeCount();
@@ -124,19 +129,18 @@ public class Graph : MonoBehaviour
     private void CheckDerivativeCount()
     {
         if (_derivativeCount < 0) _derivativeCount = 0;
+
+        if (_derivativeCount <= 0) return;
         
-        if (_derivativeCount > 0)
+        _derivativeFunction = _originalFunction;
+        for (int i = 0; i < _derivativeCount; i++)
         {
-            _derivativeFunction = _originalFunction;
-            for (int i = 0; i < _derivativeCount; i++)
-            {
-                _derivativeFunction = CalculateDerivative();
-            }
+            _derivativeFunction = CalculateDerivative();
         }
     }
 
     /// <summary>
-    /// Checks the current graph behavior for any changes.
+    /// Checks if the _previousCount is equal to the _resetCounter and performs a reset of the graph.
     /// </summary>
     private void CheckGraphReset()
     {
@@ -165,7 +169,7 @@ public class Graph : MonoBehaviour
     private void ResetGraph()
     {
         _isResetting = true;
-        _resetCounter.count = 0;
+        _previousCount = -1;
         _lineRenderer.positionCount = 0;
         _lineRendererIndex = 0;
         _transform.position = _startingPosition;
@@ -176,7 +180,7 @@ public class Graph : MonoBehaviour
     }
 
     /// <summary>
-    /// Increments the input value passed into the graph function.
+    /// Increments the input value passed into the graph function, adjusting it for _graphSpeed and the time elapsed from the last frame.
     /// </summary>
     private void UpdateInput()
     {
@@ -186,12 +190,12 @@ public class Graph : MonoBehaviour
     }
 
     /// <summary>
-    /// Tracks the output of the _originalFunction to determine when any derivatives should also stop graphing.
+    /// Tracks the output of the _originalFunction in order to stop the graph from drawing if it passes the _yPositionBoundary.
     /// </summary>
     private bool CheckGraphBounds()
     {
         Vector3 _originalMovePosition = GraphFunctionLibrary.GraphFunction(_input, _originalFunction.exponent, _originalFunction.coefficient);
-        return (_originalMovePosition.y + _originalFunction.height) < 0;
+        return (_originalMovePosition.y + _originalFunction.yIntercept) < _yPositionBoundary;
     }
     
     /// <summary>
@@ -201,26 +205,33 @@ public class Graph : MonoBehaviour
     {
         if (_isResetting) return;
 
+        //Draw either the derivative or the original function.
         _movePosition = _derivativeCount > 0 ? 
                             GraphFunctionLibrary.GraphFunction(_input, _derivativeFunction.exponent, _derivativeFunction.coefficient) : 
                             GraphFunctionLibrary.GraphFunction(_input, _originalFunction.exponent, _originalFunction.coefficient);
         
+        //Constantly update the _previousCount to determine when a function has stopped graphing.
         _previousCount = _resetCounter.count;
         
+        //If the graph has passed the _yPositionBoundary, then stop graphing.
         if (CheckGraphBounds()) return;
 
+        //Constantly increment the _resetCounter to indicate a graph is currently drawing.
         _resetCounter.count++;
             
+        //Update the positionCount to 1 value greater than the _lineRendererIndex to ensure the SetPosition method properly assigns the next position for it to render the line.
         if (_lineRenderer.positionCount <= _lineRendererIndex)
         {
             _lineRenderer.positionCount = _lineRendererIndex + 1;
         }
 
+        //Update the graph's position.
         _movePosition.y += _startingPosition.y;
-        Vector3 _updatedX = _movePosition;
-        _updatedX.x += _startingPosition.x;
+        Vector3 _updatedInput = _movePosition;
+        _updatedInput.x += _startingPosition.x;
 
-        _lineRenderer.SetPosition(_lineRendererIndex, _updatedX);
+        //Draw the line renderer.
+        _lineRenderer.SetPosition(_lineRendererIndex, _updatedInput);
         _lineRendererIndex++;
     }
 
@@ -230,16 +241,18 @@ public class Graph : MonoBehaviour
     private GraphFunctionEquation CalculateDerivative()
     {
         GraphFunctionEquation _derivative = new GraphFunctionEquation();
+        
+        //Uses the power rule for derivatives.
         _derivative.coefficient = _derivativeFunction.exponent * _derivativeFunction.coefficient;
         _derivative.exponent = _derivativeFunction.exponent - 1;
 
-        if (Math.Abs(_derivative.exponent) < Mathf.Epsilon)
+        if (Math.Abs(_derivative.exponent) <= 0)
         {
-            _derivative.height = _derivative.coefficient;
+            _derivative.yIntercept = _derivative.coefficient;
         }
         else
         {
-            _derivative.height = 0;
+            _derivative.yIntercept = 0;
         }
 
         return _derivative;
